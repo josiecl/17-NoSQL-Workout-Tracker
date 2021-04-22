7ec9d594ddf5bcc0fa3de171890c8f6a87dee2fb
Josie <josiecollinsrc@gmail.com>
2021-04-21 19:20:58 -0500

    api routes completed

---
+++ routes/api.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        {
+           $push: {exercises: body} 
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
